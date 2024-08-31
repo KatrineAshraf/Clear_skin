d9c7b204f32cbc5d6fa78206af021c3632b6a8c0
KatrineAshraf <katherineashraf@gmail.com>
2024-08-31 16:13:53 +0300

    map

--- src/App.jsx
+++ src/App.jsx
@@ -3,150 +3,113 @@ import './App.css';
 
 // Define the forbidden ingredients with synonyms and maximum percentages
 const forbiddenIngredients = {
-  'Beeswax': {
-    synonyms: ['Cera alba'],
-    maxPercentage: null
-  },
-  'Flaxseed oil': {synonyms: [], maxPercentage: null},
-  'Capric acid': {synonyms: [], maxPercentage: null},
-  'Cocoa butter': {synonyms: [], maxPercentage: null},
-  'Coconut oil': {
-    synonyms: ['Coconut alkanes', 'Coconut butter', 'Cocos nucifera'],
-    maxPercentage: null,
-  },
-  'Corn oil': {synonyms: [], maxPercentage: null},
-  'Cotton seed oil': {synonyms: [], maxPercentage: null},
-  'Eicosanoic acid': {synonyms: [], maxPercentage: null},
-  'Ethylhexyl palmitate': {synonyms: [], maxPercentage: null},
-  'Ethylhexyl pelargonate': {synonyms: [], maxPercentage: null},
-  'Evening primrose oil': {synonyms: [], maxPercentage: null},
-  'Decyl oleate': {synonyms: [], maxPercentage: null},
-  'Di (2 ethylhexyl) succinate': {
-    synonyms: ['Di succinate', 'Disuccinate', '2 ethylhexyl'],
-    maxPercentage: null
-  },
-  'Dioctyl malate': {synonyms: [], maxPercentage: null},
-  'Dioctyl succinate': {synonyms: [], maxPercentage: null},
-  'A & D additive': {
-    synonyms: ['A additive', 'D additive'],
-    maxPercentage: null
-  },
-  'Acetylated lanolin alcohol': {synonyms: [], maxPercentage: null},
-  'Almond oil': {
-    synonyms: ['Prunus amygdalus dulcis', 'Prunus dulcis (almond) oil'],
-    maxPercentage: null
-  },
-  'Apricot kernel oil': {synonyms: [], maxPercentage: null},
-  'Ascorbyl palmitate': {synonyms: [], maxPercentage: null},
-  'Avocado oil': {
-    synonyms: ['Persea gratissima (avocado) oil'],
-    maxPercentage: null
-  },
-  'Laneth-10': {synonyms: [], maxPercentage: null},
-  'Lanolic acid': {synonyms: [], maxPercentage: null},
-  'Lanolin alcohol': {synonyms: [], maxPercentage: null},
-  'Laureth-23': {synonyms: [], maxPercentage: null},
-  'Laureth-4': {synonyms: [], maxPercentage: null},
-  'Lauric acid': {synonyms: [], maxPercentage: null},
-  'Linoleic acid': {synonyms: [], maxPercentage: null},
-  'Linolenic acid': {synonyms: [], maxPercentage: null},
-  'Linseed oil': {synonyms: [], maxPercentage: null},
-  'Mineral oil': {synonyms: [], maxPercentage: null},
-  'Mink oil': {synonyms: [], maxPercentage: null},
-  'Myreth-3 myristate': {synonyms: [], maxPercentage: null},
-  'Myristic acid': {synonyms: [], maxPercentage: null},
-  'Myristyl alcohol': {synonyms: [], maxPercentage: null},
-  'Isocetyl alcohol': {synonyms: [], maxPercentage: null},
-  'Isodecyl oleate': {synonyms: [], maxPercentage: null},
-  'Isopropyl isostearate': {synonyms: [], maxPercentage: null},
-  'Isopropyl lanolate': {synonyms: [], maxPercentage: null},
-  'Isopropyl linolate': {synonyms: [], maxPercentage: null},
-  'Isopropyl myristate': {synonyms: [], maxPercentage: null},
-  'Isopropyl neopentanoate': {synonyms: [], maxPercentage: null},
-  'Isopropyl palmitate': {synonyms: [], maxPercentage: null},
-  'Isostearic acid': {synonyms: [], maxPercentage: null},
-  'Isostearyl alcohol': {synonyms: [], maxPercentage: null},
-  'Isostearyl isostearate': {synonyms: [], maxPercentage: null},
-  'Isostearyl neopentanoate': {synonyms: [], maxPercentage: null},
-  'Glyceryl stearate SE': {synonyms: [], maxPercentage: null},
-  'Glyceryl-3-diisostearate': {synonyms: [], maxPercentage: null},
-  'Glycine soya (soybean) oil': {
-    synonyms: ['Soybean oil', 'Glycine soya oil', 'soya oil'],
-    maxPercentage: null
-  },
-  'Grapeseed oil': {
-    synonyms: [
-      'Vitis vinifera (grapeseed) oil',
-      'Vitis vinifera',
-      'Vitis vinifera oil'
-    ],
-    maxPercentage: null
-  },
-  'Hexylene glycol': {synonyms: [], maxPercentage: null},
-  'Hydrogenated vegetable oil': {synonyms: [], maxPercentage: null},
-  'Myristyl lactate': {synonyms: [], maxPercentage: null},
-  'Myristyl myristate': {synonyms: [], maxPercentage: null},
-  'Palm oil': {synonyms: [], maxPercentage: null},
-  'Palmitic acid': {synonyms: [], maxPercentage: null},
-  'Peach kernel oil': {synonyms: [], maxPercentage: null},
-  'Peanut oil': {synonyms: [], maxPercentage: null},
-  'PEG 100 distearate': {synonyms: [], maxPercentage: null},
-  'PEG 150 distearate': {synonyms: [], maxPercentage: null},
-  'PEG 16 lanolin': {synonyms: [], maxPercentage: null},
-  'PEG 200 dilaurate': {synonyms: [], maxPercentage: null},
-  'PEG 8 stearate': {synonyms: [], maxPercentage: null},
-  'Pentaerythrital tetraisostearate': {synonyms: [], maxPercentage: null},
-  'PG caprylate/caprate': {synonyms: [], maxPercentage: null},
-  'PG dipelargonate': {synonyms: [], maxPercentage: null},
-  'PG monostearate': {synonyms: [], maxPercentage: null},
-  'Phytanriol': {synonyms: [], maxPercentage: null},
-  'Polyglyceryl-3-diisostearate': {synonyms: [], maxPercentage: null},
-  'PPG 10 cetyl ether': {synonyms: [], maxPercentage: null},
-  'PPG 12 PEG 65 lanolin oil': {synonyms: [], maxPercentage: null},
-  'PPG 2 myristyl propionate': {synonyms: [], maxPercentage: null},
-  'PPG 5 Ceteth 10 phosphate': {synonyms: [], maxPercentage: null},
-  'Prunus amygdalus dulcis': {
-    synonyms: ['Almond oil', 'Prunus dulcis (almond) oil'],
-    maxPercentage: null
-  },
-  'Octyl palmitate': {synonyms: [], maxPercentage: null},
-  'Octyl stearate': {synonyms: [], maxPercentage: null},
-  'Octyldodecanol': {synonyms: [], maxPercentage: null},
-  'Olea europaea (olive) fruit oil': {
-    synonyms: ['Olive oil'],
-    maxPercentage: null
-  },
-  'Oleth-10': {synonyms: [], maxPercentage: null},
-  'Oleth-3': {synonyms: [], maxPercentage: null},
-  'Oleth-3 phosphate': {synonyms: [], maxPercentage: null},
-  'Oleth-5': {synonyms: [], maxPercentage: null},
-  'Oleyl alcohol': {synonyms: [], maxPercentage: null},
-  'Xylene': {synonyms: [], maxPercentage: null},
-  'Sandalwood seed oil': {synonyms: [], maxPercentage: null},
-  'Sesame oil': {synonyms: [], maxPercentage: null},
-  'Shark liver oil': {synonyms: [], maxPercentage: null},
-  'Sorbitan isostearate': {synonyms: [], maxPercentage: null},
-  'Sorbitan laurate': {synonyms: [], maxPercentage: null},
-  'Sorbitan oleate': {synonyms: [], maxPercentage: null},
-  'Steareth-10': {synonyms: [], maxPercentage: null},
-  'Steareth-20': {synonyms: [], maxPercentage: null},
-  'Stearic acid': {synonyms: [], maxPercentage: null},
-  'Stearic acid: TEA': {synonyms: [], maxPercentage: null},
-  'Stearyl alcohol': {synonyms: [], maxPercentage: null},
-  'Stearyl heptanoate': {synonyms: [], maxPercentage: null},
-  'Sulfulated jojoba oil': {synonyms: [], maxPercentage: null},
-  'Sweet almond oil': {synonyms: [], maxPercentage: null},
-  'Water-soluble sulfur': {synonyms: [], maxPercentage: null},
-  'Wheat germ glyceride': {synonyms: [], maxPercentage: null},
-  'Wheat germ oil': {synonyms: [], maxPercentage: null},
-  'Tocopherol': {synonyms: [], maxPercentage: 10},
-  'Triethanoleamine': {synonyms: [], maxPercentage: null},
-  'Vitamin A palmitate': {synonyms: [], maxPercentage: null},
-  'Vitamin E': {synonyms: [], maxPercentage: 10},
-  'Vitis vinifera (grapeseed) oil': {
-    synonyms: ['Grapeseed oil', 'Vitis vinifera', 'Vitis vinifera oil'],
-    maxPercentage: null
-  },
+  'beeswax': {synonyms: ['cera alba'], maxPercentage: None},
+  'flaxseed oil': {synonyms: [], maxPercentage: None},
+  'capric acid': {synonyms: [], maxPercentage: None},
+  'cocoa butter': {synonyms: [], maxPercentage: None},
+  'coconut oil': {synonyms: ['coconut alkanes', 'coconut butter', 'cocos nucifera'], maxPercentage: None},
+  'corn oil': {synonyms: [], maxPercentage: None},
+  'cotton seed oil': {synonyms: [], maxPercentage: None},
+  'eicosanoic acid': {synonyms: [], maxPercentage: None},
+  'ethylhexyl palmitate': {synonyms: [], maxPercentage: None},
+  'ethylhexyl pelargonate': {synonyms: [], maxPercentage: None},
+  'evening primrose oil': {synonyms: [], maxPercentage: None},
+  'decyl oleate': {synonyms: [], maxPercentage: None},
+  'di (2 ethylhexyl) succinate': {synonyms: ['di succinate', 'disuccinate', '2 ethylhexyl'], maxPercentage: None},
+  'dioctyl malate': {synonyms: [], maxPercentage: None},
+  'dioctyl succinate': {synonyms: [], maxPercentage: None},
+  'a & d additive': {synonyms: ['a additive', 'd additive'], maxPercentage: None},
+  'acetylated lanolin alcohol': {synonyms: [], maxPercentage: None},
+  'almond oil': {synonyms: ['prunus amygdalus dulcis', 'prunus dulcis (almond) oil'], maxPercentage: None},
+  'apricot kernel oil': {synonyms: [], maxPercentage: None},
+  'ascorbyl palmitate': {synonyms: [], maxPercentage: None},
+  'avocado oil': {synonyms: ['persea gratissima (avocado) oil'], maxPercentage: None},
+  'laneth-10': {synonyms: [], maxPercentage: None},
+  'lanolic acid': {synonyms: [], maxPercentage: None},
+  'lanolin alcohol': {synonyms: [], maxPercentage: None},
+  'laureth-23': {synonyms: [], maxPercentage: None},
+  'laureth-4': {synonyms: [], maxPercentage: None},
+  'lauric acid': {synonyms: [], maxPercentage: None},
+  'linoleic acid': {synonyms: [], maxPercentage: None},
+  'linolenic acid': {synonyms: [], maxPercentage: None},
+  'linseed oil': {synonyms: [], maxPercentage: None},
+  'mineral oil': {synonyms: [], maxPercentage: None},
+  'mink oil': {synonyms: [], maxPercentage: None},
+  'myreth-3 myristate': {synonyms: [], maxPercentage: None},
+  'myristic acid': {synonyms: [], maxPercentage: None},
+  'myristyl alcohol': {synonyms: [], maxPercentage: None},
+  'isocetyl alcohol': {synonyms: [], maxPercentage: None},
+  'isodecyl oleate': {synonyms: [], maxPercentage: None},
+  'isopropyl isostearate': {synonyms: [], maxPercentage: None},
+  'isopropyl lanolate': {synonyms: [], maxPercentage: None},
+  'isopropyl linolate': {synonyms: [], maxPercentage: None},
+  'isopropyl myristate': {synonyms: [], maxPercentage: None},
+  'isopropyl neopentanoate': {synonyms: [], maxPercentage: None},
+  'isopropyl palmitate': {synonyms: [], maxPercentage: None},
+  'isostearic acid': {synonyms: [], maxPercentage: None},
+  'isostearyl alcohol': {synonyms: [], maxPercentage: None},
+  'isostearyl isostearate': {synonyms: [], maxPercentage: None},
+  'isostearyl neopentanoate': {synonyms: [], maxPercentage: None},
+  'glyceryl stearate se': {synonyms: [], maxPercentage: None},
+  'glyceryl-3-diisostearate': {synonyms: [], maxPercentage: None},
+  'glycine soya (soybean) oil': {synonyms: ['soybean oil', 'glycine soya oil', 'soya oil'], maxPercentage: None},
+  'grapeseed oil': {synonyms: ['vitis vinifera (grapeseed) oil', 'vitis vinifera', 'vitis vinifera oil'], maxPercentage: None},
+  'hexylene glycol': {synonyms: [], maxPercentage: None},
+  'hydrogenated vegetable oil': {synonyms: [], maxPercentage: None},
+  'myristyl lactate': {synonyms: [], maxPercentage: None},
+  'myristyl myristate': {synonyms: [], maxPercentage: None},
+  'palm oil': {synonyms: [], maxPercentage: None},
+  'palmitic acid': {synonyms: [], maxPercentage: None},
+  'peach kernel oil': {synonyms: [], maxPercentage: None},
+  'peanut oil': {synonyms: [], maxPercentage: None},
+  'peg 100 distearate': {synonyms: [], maxPercentage: None},
+  'peg 150 distearate': {synonyms: [], maxPercentage: None},
+  'peg 16 lanolin': {synonyms: [], maxPercentage: None},
+  'peg 200 dilaurate': {synonyms: [], maxPercentage: None},
+  'peg 8 stearate': {synonyms: [], maxPercentage: None},
+  'pentaerythrital tetraisostearate': {synonyms: [], maxPercentage: None},
+  'pg caprylate/caprate': {synonyms: [], maxPercentage: None},
+  'pg dipelargonate': {synonyms: [], maxPercentage: None},
+  'pg monostearate': {synonyms: [], maxPercentage: None},
+  'phytanriol': {synonyms: [], maxPercentage: None},
+  'polyglyceryl-3-diisostearate': {synonyms: [], maxPercentage: None},
+  'ppg 10 cetyl ether': {synonyms: [], maxPercentage: None},
+  'ppg 12 peg 65 lanolin oil': {synonyms: [], maxPercentage: None},
+  'ppg 2 myristyl propionate': {synonyms: [], maxPercentage: None},
+  'ppg 5 ceteth 10 phosphate': {synonyms: [], maxPercentage: None},
+  'prunus amygdalus dulcis': {synonyms: ['almond oil', 'prunus dulcis (almond) oil'], maxPercentage: None},
+  'octyl palmitate': {synonyms: [], maxPercentage: None},
+  'octyl stearate': {synonyms: [], maxPercentage: None},
+  'octyldodecanol': {synonyms: [], maxPercentage: None},
+  'olea europaea (olive) fruit oil': {synonyms: ['olive oil'], maxPercentage: None},
+  'oleth-10': {synonyms: [], maxPercentage: None},
+  'oleth-3': {synonyms: [], maxPercentage: None},
+  'oleth-3 phosphate': {synonyms: [], maxPercentage: None},
+  'oleth-5': {synonyms: [], maxPercentage: None},
+  'oleyl alcohol': {synonyms: [], maxPercentage: None},
+  'xylene': {synonyms: [], maxPercentage: None},
+  'sandalwood seed oil': {synonyms: [], maxPercentage: None},
+  'sesame oil': {synonyms: [], maxPercentage: None},
+  'shark liver oil': {synonyms: [], maxPercentage: None},
+  'sorbitan isostearate': {synonyms: [], maxPercentage: None},
+  'sorbitan laurate': {synonyms: [], maxPercentage: None},
+  'sorbitan oleate': {synonyms: [], maxPercentage: None},
+  'steareth-10': {synonyms: [], maxPercentage: None},
+  'steareth-20': {synonyms: [], maxPercentage: None},
+  'stearic acid': {synonyms: [], maxPercentage: None},
+  'stearic acid: tea': {synonyms: [], maxPercentage: None},
+  'stearyl alcohol': {synonyms: [], maxPercentage: None},
+  'stearyl heptanoate': {synonyms: [], maxPercentage: None},
+  'sulfulated jojoba oil': {synonyms: [], maxPercentage: None},
+  'sweet almond oil': {synonyms: [], maxPercentage: None},
+  'water-soluble sulfur': {synonyms: [], maxPercentage: None},
+  'wheat germ glyceride': {synonyms: [], maxPercentage: None},
+  'wheat germ oil': {synonyms: [], maxPercentage: None},
+  'tocopherol': {synonyms: [], maxPercentage: 10},
+  'triethanoleamine': {synonyms: [], maxPercentage: None},
+  'vitamin a palmitate': {synonyms: [], maxPercentage: None},
+  'vitamin e': {synonyms: [], maxPercentage: 10},
+  'vitis vinifera (grapeseed) oil': {synonyms: ['grapeseed oil', 'vitis vinifera', 'vitis vinifera oil'], maxPercentage: None}
 };
 
 const App = () => {
@@ -154,45 +117,55 @@ const App = () => {
   const [violations, setViolations] = useState([]);
 
   // Function to validate ingredients and percentages
-  const validateIngredients = (ingredientList) => {
-    const violations = [];
-    
-    // Splitting and parsing input to extract ingredient names and their percentages if available
-    const ingredientArray = ingredientList.split(',').map(item => item.trim());
-    ingredientArray.forEach(entry => {
-      const [ingredient, percentage] = entry.split(/\s+/);
-      const percentageValue = parseFloat(percentage);
+// Function to validate ingredients and percentages
+const validateIngredients = (ingredientList) => {
+  const violations = [];
+
+  // Splitting and parsing input to extract ingredient names and their percentages if available
+  const ingredientArray = ingredientList.split(',').map(item => item.trim());
+  console.log(ingredientArray)
+  ingredientArray.forEach(entry => {
+    // Check for percentages; assumes format like "IngredientName 5%" or just "IngredientName"
+    const match = entry.match(/(.+?)\s*([\d.]+%)?$/);
+    if (match) {
+      const ingredient = match[1].trim().toLowerCase();
+      const percentageValue = match[2] ? parseFloat(match[2]) : null;
 
       // Check exact matches and synonyms, and handle presence without specific percentage
       checkIngredientViolation(ingredient, percentageValue, violations);
-    });
+    }
+  });
 
-    return violations;
-  };
+  return violations;
+};
 
-  const checkIngredientViolation = (ingredient, percentageValue, violations) => {
-    // Check exact matches
-    if (forbiddenIngredients[ingredient]) {
-      const { maxPercentage } = forbiddenIngredients[ingredient];
-      if (maxPercentage !== null && !isNaN(percentageValue) && percentageValue > maxPercentage) {
-        violations.push(`${ingredient} exceeds the allowed limit of ${maxPercentage}%`);
+const checkIngredientViolation = (ingredient, percentageValue, violations) => {
+  // Check exact matches, converting keys to lowercase for case-insensitive match
+  const normalizedIngredient = ingredient.toLowerCase();
+  
+  if (forbiddenIngredients[normalizedIngredient]) {
+    const { maxPercentage } = forbiddenIngredients[normalizedIngredient];
+    if (maxPercentage !== null && percentageValue !== null && percentageValue > maxPercentage) {
+      violations.push(`${ingredient} exceeds the allowed limit of ${maxPercentage}%`);
+    } else if (maxPercentage === null) {
+      violations.push(`${ingredient} is not allowed`);
+    }
+  }
+
+  // Check synonyms, ensuring case-insensitivity
+  Object.entries(forbiddenIngredients).forEach(([key, value]) => {
+    const normalizedKey = key.toLowerCase();
+    if (value.synonyms.map(s => s.toLowerCase()).includes(normalizedIngredient)) {
+      const { maxPercentage } = value;
+      if (maxPercentage !== null && percentageValue !== null && percentageValue > maxPercentage) {
+        violations.push(`${ingredient} (synonym for ${key}) exceeds the allowed limit of ${maxPercentage}%`);
       } else if (maxPercentage === null) {
-        violations.push(`${ingredient} is not allowed`);
+        violations.push(`${ingredient} (synonym for ${key}) is not allowed`);
       }
     }
+  });
+};
 
-    // Check synonyms
-    Object.entries(forbiddenIngredients).forEach(([key, value]) => {
-      if (value.synonyms.includes(ingredient)) {
-        const { maxPercentage } = value;
-        if (maxPercentage !== null && !isNaN(percentageValue) && percentageValue > maxPercentage) {
-          violations.push(`${ingredient} (synonym for ${key}) exceeds the allowed limit of ${maxPercentage}%`);
-        } else if (maxPercentage === null) {
-          violations.push(`${ingredient} (synonym for ${key}) is not allowed`);
-        }
-      }
-    });
-  };
 
   // Handle form submission
   const handleSubmit = (e) => {
